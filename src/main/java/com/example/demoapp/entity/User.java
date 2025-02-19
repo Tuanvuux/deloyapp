38824c10a6f66a0f572e447c3c41270b29a09b7d
Tuan Vu <Nguyentuanvu1410@gmail.com>
2025-02-19 10:19:53 +0700

    commit

---
+++ src/main/java/com/example/demoapp/entity/User.java
@@ -0,0 +1,14 @@
+package com.example.demoapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+}
